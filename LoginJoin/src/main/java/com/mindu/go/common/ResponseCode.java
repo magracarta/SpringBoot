7c60c99f3a0a9828c249eb147a29f1bbf79f97f5
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2024-06-22 22:30:57 +0900

    dto생성

---
+++ LoginJoin/src/main/java/com/mindu/go/common/ResponseCode.java
@@ -0,0 +1,12 @@
+package com.mindu.go.common;
+
+public interface ResponseCode {
+    String SUCCESS = "SU";
+    String VALIDATION_FAILED = "VF";
+    String DUPLICATION_ID = "DI";
+    String SIGN_IN_FAIL = "SF";
+    String CERTIFICATION_FAIL = "CF";
+    String DATABASE_ERROR = "DBE";
+
+
+}
---
+++ LoginJoin/src/main/java/com/mindu/go/common/ResponseMessage.java
@@ -0,0 +1,11 @@
+package com.mindu.go.common;
+
+public interface ResponseMessage {
+    String SUCCESS = "Success.";
+    String VALIDATION_FAILED = "Validation failed.";
+    String DUPLICATION_ID = "Duplicate Id.";
+    String SIGN_IN_FAIL = "Login information mismatch.";
+    String CERTIFICATION_FAIL = "Certification failed.";
+    String DATABASE_ERROR = "Database error.";
+
+}
